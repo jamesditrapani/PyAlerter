11f3734136e18fb52484e0cb0a8a2b05c0e095cc
James Di Trapani <51264648+jamesditrapani@users.noreply.github.com>
2019-06-25 07:20:32 +1000

    Update python_alerter.py

--- python_alerter.py
+++ python_alerter.py
@@ -8,7 +8,6 @@ class alerter():
 	def format_alert(email_to, email_from, email_subject, alert_severity, alert_summary, alert_host, alert_module, alert_timestamp, alert_data):
 		with open('alert_template.html', 'r') as alert_template:
 			html_template = alert_template.read().format(alert_severity, alert_summary, alert_host, alert_module, alert_timestamp, alert_data)
-			#html_template = alert_template.read()
 			alerter.send_alert(email_to, email_from, email_subject, html_template)
 
 	def send_alert(email_to, email_from, email_subject, html_template):
